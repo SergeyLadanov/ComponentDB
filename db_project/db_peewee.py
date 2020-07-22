dd94dea45b3a437c4625e44f347bf8f45d1ff9ae
Sergey Ladanov <ladanov@digitrans.ru>
2020-07-22 14:45:05 +0300

    Пример

--- db_project/db_peewee.py
+++ db_project/db_peewee.py
@@ -34,7 +34,32 @@ class Pet(BaseModel):
 
 dbhandle.connect()
 
+#uncle_bob = Person(name='Ваня', birthday=date(1960, 1, 15))
+#uncle_bob.save() # bob is now stored in the database
+#dbhandle.create_tables([Person, Pet])
+
 uncle_bob = Person(name='Bob', birthday=date(1960, 1, 15))
 uncle_bob.save() # bob is now stored in the database
-#dbhandle.create_tables([Person, Pet])
+grandma = Person.create(name='Grandma', birthday=date(1935, 3, 1))
+herb = Person.create(name='Herb', birthday=date(1950, 5, 5))
+
+grandma.name = 'Grandma L.'
+grandma.save()  # Update grandma's name in the database.
+
+bob_kitty = Pet.create(owner=uncle_bob, name='Kitty', animal_type='cat')
+herb_fido = Pet.create(owner=herb, name='Fido', animal_type='dog')
+herb_mittens = Pet.create(owner=herb, name='Mittens', animal_type='cat')
+herb_mittens_jr = Pet.create(owner=herb, name='Mittens Jr', animal_type='cat')
+
+herb_mittens.delete_instance() # he had a great life
+
+herb_fido.owner = uncle_bob
+herb_fido.save()
+
+for person in Person.select():
+    print(person.name)
+
+
 print("Test")
+
+dbhandle.close()
