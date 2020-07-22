6187214b86d62732cdc41af7d247ddee450c4d9a
Sergey Ladanov <ladanov@digitrans.ru>
2020-07-22 16:55:40 +0300

    Пример чтения значений из базы

--- db_project/db_peewee.py
+++ db_project/db_peewee.py
@@ -38,26 +38,26 @@ dbhandle.connect()
 #uncle_bob.save() # bob is now stored in the database
 #dbhandle.create_tables([Person, Pet])
 
-uncle_bob = Person(name='Bob', birthday=date(1960, 1, 15))
-uncle_bob.save() # bob is now stored in the database
-grandma = Person.create(name='Grandma', birthday=date(1935, 3, 1))
-herb = Person.create(name='Herb', birthday=date(1950, 5, 5))
+#uncle_bob = Person(name='Bob', birthday=date(1960, 1, 15))
+#uncle_bob.save() # bob is now stored in the database
+#grandma = Person.create(name='Grandma', birthday=date(1935, 3, 1))
+#herb = Person.create(name='Herb', birthday=date(1950, 5, 5))
 
-grandma.name = 'Grandma L.'
-grandma.save()  # Update grandma's name in the database.
+#grandma.name = 'Grandma L.'
+#grandma.save()  # Update grandma's name in the database.
 
-bob_kitty = Pet.create(owner=uncle_bob, name='Kitty', animal_type='cat')
-herb_fido = Pet.create(owner=herb, name='Fido', animal_type='dog')
-herb_mittens = Pet.create(owner=herb, name='Mittens', animal_type='cat')
-herb_mittens_jr = Pet.create(owner=herb, name='Mittens Jr', animal_type='cat')
+#bob_kitty = Pet.create(owner=uncle_bob, name='Kitty', animal_type='cat')
+#herb_fido = Pet.create(owner=herb, name='Fido', animal_type='dog')
+#herb_mittens = Pet.create(owner=herb, name='Mittens', animal_type='cat')
+#herb_mittens_jr = Pet.create(owner=herb, name='Mittens Jr', animal_type='cat')
 
-herb_mittens.delete_instance() # he had a great life
+#herb_mittens.delete_instance() # he had a great life
 
-herb_fido.owner = uncle_bob
-herb_fido.save()
-
-for person in Person.select():
-    print(person.name)
+#herb_fido.owner = uncle_bob
+#herb_fido.save()
+query = Pet.select().where(Pet.animal_type == 'cat', Pet.name == 'Kitty')
+for pet in query:
+    print(pet.name, pet.owner.name)
 
 
 print("Test")
