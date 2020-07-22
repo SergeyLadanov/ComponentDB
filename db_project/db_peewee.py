7cf3cc721bc41c1858f785866e8bf4fb0afa2545
Sergey Ladanov <ladanov@digitrans.ru>
2020-07-22 14:36:09 +0300

    Создание таблицы через peewee

--- db_project/db_peewee.py
+++ db_project/db_peewee.py
@@ -1,10 +1,11 @@
 import peewee
 from peewee import *
+from datetime import date
 
  
 user = 'root'
 password = '12345678'
-db_name = 'Components'
+db_name = 'test'
  
 dbhandle = MySQLDatabase(
     db_name, user=user,
@@ -13,5 +14,27 @@ dbhandle = MySQLDatabase(
 )
 
 
+class BaseModel(Model):
+    """A base model that will use our Sqlite database."""
+    class Meta:
+        database = dbhandle
+
+class Person(BaseModel):
+    name = CharField()
+    birthday = DateField()
+
+
+class Pet(BaseModel):
+    owner = ForeignKeyField(Person, backref='pets')
+    name = CharField()
+    animal_type = CharField()
+
+
+
+
 dbhandle.connect()
+
+uncle_bob = Person(name='Bob', birthday=date(1960, 1, 15))
+uncle_bob.save() # bob is now stored in the database
+#dbhandle.create_tables([Person, Pet])
 print("Test")
