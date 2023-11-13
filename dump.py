a01e4789cf2693f4c17f00d64026fd1227a3af71
Sergey <61871201+SergeyLadanov@users.noreply.github.com>
2023-11-13 23:22:06 +0300

    Update dump.py

--- dump.py
+++ dump.py
@@ -8,4 +8,4 @@ if config.RELATIVE_PATH:
 else:
     path = config.DUMP_PATH
 
-os.system("mysqldump" + ' ' + '--host=' + config.DB_HOST + ' ' + '--port=' + config.DB_PORT + ' ' + "--user=" + config.DB_USER + ' ' + "--password=" + config.DB_PSWD + ' ' + config.DB_NAME + " > " + path) #--no-tablespaces
+os.system("mysqldump" + ' ' + '--host=' + config.DB_HOST + ' ' + '--port=' + str(config.DB_PORT) + ' ' + "--user=" + config.DB_USER + ' ' + "--password=" + config.DB_PSWD + ' ' + config.DB_NAME + " > " + path + ' --no-tablespaces')
